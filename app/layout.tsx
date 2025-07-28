afbfff7bef599973b2faf0eefb4bbf54e6666587
Debjit Das <debjit.das@molgroup.com>
2025-07-28 10:00:11 +0530

    Update app weather icon

--- app/layout.tsx
+++ app/layout.tsx
@@ -15,11 +15,6 @@ const geistMono = Geist_Mono({
 export const metadata: Metadata = {
   title: "Marine Weather Forecast App",
   description: "Accurate surf and marine forecasts by location.",
-  icons: [
-    { rel: "icon", url: "/weather-icon.ico", type: "image/x-icon" },
-    { rel: "icon", url: "/favicon.ico", type: "image/x-icon" },
-    { rel: "icon", url: "/weather-icon.png", type: "image/png" },
-  ],
 };
 
 export default function RootLayout({
